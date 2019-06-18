c27c997c5b5b742a61edd1483e06ace3c036e111
Liz Kelley and Todd Aden <todd@>
2019-06-18 11:54:06 -0700

    add message componenet

--- src/app/messages.ts
+++ src/app/messages.ts
@@ -3,11 +3,13 @@ export class Messages {
     posts: Message[] = [];
     addPosts(post: Message) {
         post.id = Messages.id++;
+        post.timeStamp = Date.now();
         this.posts.push(post);
     }
 }
 
 export class Message {
     id: number;
+    timeStamp: number;
     constructor(public author: string, public message: string) {}
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Messages, Message } from '../messages';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
+  posts: Messages;
+  postMessage: string;
   constructor() { }
 
   ngOnInit() {
+    this.posts = new Messages();
   }
 
+  post() {
+    this.posts.addPosts(new Message(
+      "Pillow Guy",
+      this.postMessage
+    ))    
+  }
 }
